0d97cb32a3985f9b092ed2cd3ddaea95dcf80052
Darren James <darrenjames.it@gmail.com>
2023-04-23 17:08:39 +0300

    folder creation added

--- file_sorter.py
+++ file_sorter.py
@@ -2,7 +2,35 @@ import os
 
 path = input("please enter the starting directory: ")
 
-file_list = os.walk(path)
+file_list = os.listdir(path)
 
-for root, directory, file in file_list:
-    print(file)
+file_paths = []
+
+file_types = {
+    'Documents': ['.doc', '.docx', '.pdf', '.txt'],
+    'Slideshows': ['.ppt', '.pptx'],
+    'Spreadsheets': ['.xlsx', '.csv'],
+    'Images': ['.jpg', '.jpeg', 'png', '.'],
+    'Videos': ['.mp4', '.avi', '.wmv', '.m4v'],
+    'Audio': ['.mp3', '.wav', '.wav', '.wma'],
+    'Program Files': ['.css', '.py', '.js', '.html'],
+    'Other': []
+}
+
+
+def file_sorter(root_directory):
+    """First creates folders"""
+    for key in file_types:
+        # accesses dictionary keys to create folders with relevant names
+        folder = root_directory + "/" + key
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+
+    for file in file_list:
+        file_paths.append(os.path.join(root_directory, file))
+        print(file)
+
+    print(file_paths)
+
+
+file_sorter(path)
